26be314718648c142619beaf04fe0be73b258f13
Juan Ferreiro Fernandez <jferreiro@Juans-MacBook-Air.local>
2024-08-03 15:30:59 +0200

    version 03/08_2

--- src/api.js
+++ src/api.js
@@ -1,37 +1,24 @@
-const API_URL = 'http://feriantechat.fmemgncsf3e7f5aa.spaincentral.azurecontainer.io';
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+
+const API_URL = 'https://ia-chat-app-b370ea980606.herokuapp.com';
 
 const startConversation = (question, onMessage) => {
-    const eventSource = new EventSource(`${API_URL}/conversations/response`);
-
-    eventSource.onopen = () => {
-        console.log('Connection to server opened.');
-
-        // Send the initial question
-        fetch(`${API_URL}/conversations/response`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ question }),
-            mode:'cors'
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-        }).catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-    };
-
-    eventSource.onmessage = (event) => {
-        const cleanedData = event.data.replace(/^data:\s*/, ''); // Remove 'data: ' prefix
-        onMessage(cleanedData); // Continuously update with the cleaned text
-    };
-
-    eventSource.onerror = (err) => {
-        console.error('There was an error from the server', err);
-        eventSource.close();
-    };
+    fetchEventSource(`${API_URL}/conversations/start`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'text/event-stream',
+        },
+        body: JSON.stringify({ question }),
+        onmessage(event) {
+            const cleanedData = event.data.replace(/^data:\s*/, ''); // Remove 'data: ' prefix
+            console.log(cleanedData + cleanedData.length);
+            onMessage(cleanedData); // Continuously update with the cleaned text
+        },
+        onerror(err) {
+            console.error('There was an error from the server', err);
+        },
+    });
 };
 
 export { startConversation };
